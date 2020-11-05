57a4c6afdc7bcac8bf8cfbe789b315a143a498ec
ValentinaNovarino <valenovarino@gmail.com>
2020-11-05 18:58:16 +0100

    assegnato colore alle icone

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
     /*  *****milestone 1******
     Definire un array di oggetti; ogni oggetto
@@ -6,6 +7,11 @@ $(document).ready(function() {
     Utilizzando la funzione forEach e il template
     literal, visualizzare in pagina tutte le icone con il
     proprio nome.*/
+
+    /*  *****milestone 2*****:
+    definire un array di colori e associare ad ogni
+    tipo di icona un colore.
+    Visualizzare le icone di colore diverso in base al tipo.*/
 
     // definire array di oggetti
     const icons = [
@@ -83,41 +89,35 @@ $(document).ready(function() {
         }
     ];
 
-    // visualizzare in pagin tutte le icone
-    // destrutturo e recupero le chiavi che mi servono per stampare le icone in pagina in ciclo forEach per scorrere tutti gli oggetti icon
-    icons.forEach((icon) => {
-        const {name, prefix, family} = icon;
-        console.log(name, prefix, family);
-
-        // stampo in pagina con il template literal
-        $('#icons-container').append(`
-            <i class='${family} ${prefix}-${name} fa-2x'</i>
-            <p>${name}</p>
-        `);
-    });
-
-    /*  *****milestone 2*****:
-    definire un array di colori e associare ad ogni
-    tipo di icona un colore.
-    Visualizzare le icone di colore diverso in base al tipo.*/
-
     // creo l'array di colori
-    const colors = ['orange', 'green', 'blu'];
+    const colors = ['orange', 'green', 'lightblue'];
 
-    // recupero tutti i tipi di icona con un ciclo forEach
-    // creo una variabile per i tipi di icona
     const iconTypes = [];
     // ciclo le icone
     icons.forEach((icon) => {
-    // destrutturo icon e recupero il tipo di icona per ogni icona corrente
-    const {type} = icon;
+        // destrutturo icon e recupero il tipo di icona per ogni icona corrente
+        const {type} = icon;
 
-    if(!iconTypes.includes(type)) {
-        // se il tipo di icona dell'elemento corrente non è presente nell'array iconTypes lo inserisco altrimenti passo all'icona successiva
-        iconTypes.push(type);
-    };
-});
-console.log(iconTypes);
-
+        if(!iconTypes.includes(type)) {
+            // se il tipo di icona dell'elemento corrente non è presente nell'array iconTypes lo inserisco altrimenti passo  all'icona successiva
+            iconTypes.push(type);
+        };
+    });
+    console.log(iconTypes);
+    // visualizzare in pagin tutte le icone
+    // destrutturo e recupero le chiavi che mi servono per stampare le icone in pagina in ciclo forEach per scorrere tutti gli oggetti icon
+    icons.forEach((icon) => {
+        const {name, prefix, family, type} = icon;
+        // console.log(name, prefix, family);
+        const typeIndex = iconTypes.indexOf(type);
+        console.log(typeIndex, name);
+        const iconColor = colors[typeIndex];
+        console.log(iconColor);
+        // stampo in pagina con il template literal
+        $('#icons-container').append(`
+            <i class='${family} ${prefix}-${name} fa-2x' style='color:${iconColor}'</i>
+            <p>${name}</p>
+        `);
+    });
 
 });
